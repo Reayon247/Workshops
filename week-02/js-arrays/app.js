2c28a2fa747630b53af8a87ba495d334cab6fde1
Ryan Nugent <ryan.nugent247@gmail.com>
2025-05-06 10:38:08 +0100

    array workshop

---
+++ week-02/js-arrays/app.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+  "My fourth blog post",
+];
+
+console.log(blogPosts);
+// Arrays start at 0
+console.log(blogPosts[2]);
+
+const favouriteFood = ["cake", "roast", "bakewell tarts"];
+
+console.log(favouriteFood);
+
+favouriteFood.unshift("Chocolate Orange");
+
+console.log(favouriteFood);
+
+// unshfit add to front, shift remove from front
+// push add to back, pop remove from back
+
+const favouriteNumbers = [1, 2, 4, 6, 2, 6, 7, true];
+
+console.log(favouriteNumbers);
